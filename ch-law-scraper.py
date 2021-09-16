7f62542e8df1f870da353050be6a375a78d5db40
quadratecode <email@example.com47365835+quadratecode@users.noreply.github.com>
2021-09-16 19:26:56 +0200

    some more code

--- ch-law-scraper.py
+++ ch-law-scraper.py
@@ -7,60 +7,72 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from markdownify import markdownify
-
-# target URL
-url = "https://www.fedlex.admin.ch/eli/cc/24/233_245_233/en"
+import time
 
 # utilize headless browser
 opts = FirefoxOptions()
 opts.add_argument("--headless")
 driver = webdriver.Firefox(
     executable_path=GeckoDriverManager().install(), options=opts)
-driver.get(url)
 
-try:
-    delay = 20  # 20 second delay
-    WebDriverWait(driver, delay).until(
-        EC.visibility_of_element_located((By.ID, 'lawcontent')))
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    lawtext = soup.find("div", id="lawcontent")  # extract norms
+with open('linklist.txt', 'r') as f_in:
+    for line in map(str.strip, f_in):
+        if not line:
+            continue
 
-    # remove toolbar
-    soup.find("div", id="toolbar").decompose()
+        # get html
+        driver.get(line)
+        time.sleep(10)  # standard delay
+        delay = 60  # max delay in seconds until element is present
+        WebDriverWait(driver, delay).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, 'srnummer')))
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
 
-    # remove footnotes
-    for div in soup.find_all("div", {'class': 'footnotes'}):
-        div.decompose()
+        # get norms
+        lawtext = soup.find("div", id="lawcontent")
 
-    # remove superscripts belonging to footnotes
-    for superscripts in soup.find_all("sup"):
-        for suplink in superscripts.find_all("a"):
-            suplink.decompose()
+        # remove toolbar
+        soup.find("div", id="toolbar").decompose()
 
-    # isolate descriptive lists to paragraphs
-    for dt_list in soup.find_all("dl"):
-        dt_list.name = "p"
+        # remove footnotes
+        for div in soup.find_all("div", {'class': 'footnotes'}):
+            div.decompose()
 
-    for dt_list in soup.find_all("dt"):
-        dt_list.name = "br"
+        # remove superscripts belonging to footnotes
+        for superscripts in soup.find_all("sup"):
+            for suplink in superscripts.find_all("a"):
+                suplink.decompose()
 
-    # replace div headings headers with h2 headers
-    for divheader in soup.find_all("div", {'class': 'heading'}):
-        divheader.name = "h2"
+        # isolate descriptive lists to paragraphs
+        for dt_list in soup.find_all("dl"):
+            dt_list.name = "p"
 
-    # replace h6 headers with h3 headers
-    for header in soup.find_all("h6"):
-        header.name = "h3"
+        for dt_list in soup.find_all("dt"):
+            dt_list.name = "br"
 
-    # convert to markdown
-    content = markdownify(str(lawtext), heading_style='ATX')
+        # replace div headings headers with h2 headers
+        for divheader in soup.find_all("div", {'class': 'heading'}):
+            divheader.name = "h2"
 
-    # write to markdown file
-    with open("output.md", "w") as f:
-        f.write(content)
+        # replace h6 headers with h3 headers
+        for header in soup.find_all("h6"):
+            header.name = "h3"
 
+        # convert to markdown
+        content = markdownify(str(lawtext), heading_style='ATX')
 
-# raises Exception if element is not visible within delay duration
-except TimeoutException:
-    print("Timed out!")
+        # fetch SR number
+        srnum = soup.find("p", {'class': 'srnummer'})
+        if not srnum:  # <== check for NoneType
+            print('element not found')
+            srnum = 'no text'
+        else:
+            srnum = srnum.text
+
+        # write to markdown file
+        with open(srnum + ".md", "w") as f_out:
+            f_out.write(content)
+            f_out.close()
+
+f_in.close()
